9393af9513fdea288097f4a44ed194fe08d7ce46
Toyash <toyashpatil17@gmail.com>
2024-10-20 16:18:48 +0530

    refact:folder struct

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,13 +5,14 @@ import { AiOutlineHeart } from 'react-icons/ai';
 import Profile from '../assets/nav/profile.svg'
 import Home from '../assets/nav/home.svg'
 import Heart from '../assets/nav/heart.svg'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 
 const Navbar = () => {
+    const navigate=useNavigate();
     return (
-        <div className="fixed bottom-0 w-full bg-white drop-shadow-2xl  py-4 flex justify-between items-center px-8 shadow-2xl">
+        <div className="fixed bottom-0 w-full space-x-[50px] bg-white drop-shadow-2xl  py-4 flex justify-between items-center px-8 shadow-2xl">
             <div className="text-center">
                 <img src={Home} alt="Home" />
             </div>
@@ -24,11 +25,13 @@ const Navbar = () => {
             <div className="text-center">
 
             </div>
-            <Link to='/cart' >
-                <div className="text-center bg-[#6CC51D] p-[25px] absolute shadow-md drop-shadow-md right-2 bottom-4 rounded-full ">
-                    <FiShoppingCart />
-                </div>
-            </Link>
+
+            <div onClick={()=>navigate('/cart')} className="text-center bg-[#6CC51D] p-[25px] absolute shadow-md drop-shadow-md right-2 bottom-4 rounded-full ">
+                <FiShoppingCart />
+            </div>
+
+
+
         </div>
     )
 }
